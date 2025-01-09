3672e3441a8421ae9ef4d253af222a24535215f4
gv646 <surolia.gunjan.96@gmail.com>
2025-01-09 10:30:39 +1000

    Exceptiuon handling implemented

--- src/main/java/com/productservice/productservice/services/ProductService.java
+++ src/main/java/com/productservice/productservice/services/ProductService.java
@@ -2,13 +2,14 @@ package com.productservice.productservice.services;
 
 import com.productservice.productservice.dtos.FakeStoreProductDto;
 import com.productservice.productservice.dtos.GenericProductDto;
+import com.productservice.productservice.exceptions.ProductNotFoundException;
 import com.productservice.productservice.models.Product;
 
 import java.util.List;
 
 public interface ProductService {
 
-    GenericProductDto getProductById(Long productId);
+    GenericProductDto getProductById(Long productId) throws ProductNotFoundException;
 
     List<GenericProductDto> getAllProducts();
 
