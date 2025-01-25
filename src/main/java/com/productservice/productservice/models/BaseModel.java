65e4721bb57d94efa9c7f7ced55e98497fee4a71
gv646 <surolia.gunjan.96@gmail.com>
2025-01-25 09:21:05 +0530

    implemented UUID

--- src/main/java/com/productservice/productservice/models/BaseModel.java
+++ src/main/java/com/productservice/productservice/models/BaseModel.java
@@ -1,9 +1,15 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -11,5 +17,8 @@ import lombok.Setter;
 public class BaseModel {
 
     @Id
-    private Long id;
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator", strategy = "uuid2")
+    @Column(name = "id",columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID id;
 }
